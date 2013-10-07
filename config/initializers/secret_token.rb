eaa6164e7adb3108ac733520309ca4f58af1c8bc
AxleMaxGit <spam@thefunkyway.com>
2013-10-07 13:00:28 +1100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '9b9bbe83103759a05657882c5455cadfd3cc9b605b88aa07d446248d5671599a45f4db2f478676ef2d5155b41990c0575e0fc31ce711aceb31d7e8080f50e9e8'
